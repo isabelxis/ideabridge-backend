30799412ab604cc753ac54780a088b6879739c8e
ixsilva <ixsilva@sfiec.org.br>
2026-01-16 18:47:49 -0300

    initial project

---
+++ src/main/java/com/ideabridge_backend/ideabridge_backend/IdeabridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ideabridge_backend.ideabridge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeabridgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeabridgeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideabridge_backend/ideabridge_backend/IdeabridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideabridge_backend.ideabridge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeabridgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
